67ff55c0d647290c502a6c2b799f8a2a8cdeac30
Abdelmounim Baouia <abdelmounimbaouia@gmail.com>
2023-12-22 03:22:14 +0100

    fix: updated trips to use location as a foreing key instead of a fixed value, also added passengers

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -4,14 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Trip extends Model
 {
     protected $fillable = ['start_location', 'destination', 'departure_time', 'driver_id', 'available_seats'];
+
+    public function startLocation(): HasOne
+    {
+        return $this->HasOne(Location::class);
+    }
+
+    public function destination(): HasOne
+    {
+        return $this->HasOne(Location::class);
+    }
+
     public function driver(): HasOne
     {
         return $this->HasOne(User::class);
     }
+
+    public function passengers(): HasMany
+    {
+        return $this->HasMany(User::class);
+    }
+
     use HasFactory;
 }
