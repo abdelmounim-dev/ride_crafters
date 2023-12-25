b422ee2159cc5ffab523f2f779170b3bda457d98
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2023-12-25 18:58:01 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,24 +20,24 @@ use App\Http\Controllers\LocationController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     
 Route::apiResource('trips', TripController::class);
-Route::post('/trips', [TripController::class, 'store']);
-Route::get('/trips/{trips}', [TripController::class, 'show']);
-Route::post('/trips/{trips}/accept', [TripController::class, 'accept']);
-Route::post('/trips/{trips}/start', [TripController::class, 'start']);
-Route::post('/trips/{trips}/end', [TripController::class, 'end']);
-Route::post('/trips/{trips}/location', [TripController::class, 'location']);
+Route::post('/api/trips', [TripController::class, 'store']);
+Route::get('/api/trips/{trips}', [TripController::class, 'show']);
+Route::post('/api/trips/{trips}/accept', [TripController::class, 'accept']);
+Route::post('/api/trips/{trips}/start', [TripController::class, 'start']);
+Route::post('/api/trips/{trips}/end', [TripController::class, 'end']);
+Route::post('/api/trips/{trips}/location', [TripController::class, 'location']);
 
-Route::apiResource('locations', LocationController::class);
-Route::apiResource('reservations', ReservationController::class);
+Route::apiResource('/api/locations', LocationController::class);
+Route::apiResource('/api/reservations', ReservationController::class);
 
-Route::get('/driver', [DriverController::class, 'show']);
-Route::post('/driver', [DriverController::class, 'update']);
+Route::get('/api/driver', [DriverController::class, 'show']);
+Route::post('/api/driver', [DriverController::class, 'update']);
 
     return $request->user();
 });
 
-Route::post("/register", [AuthController::class, 'register']);
-Route::post("/login", [AuthController::class, 'login']);
+Route::post("/api/register", [AuthController::class, 'register']);
+Route::post("/api/login", [AuthController::class, 'login']);
 
 
 Route::get('/api/admin/trips', [AdminController::class,'showAllTrips']);
