2e2d2bb3104662884875eac0156f706b478aed7b
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2023-12-25 18:47:31 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,5 +10,38 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+
+    public function showAllTrips()
+    {
+        // Fetch all trips from the database
+        $trips = Trip::all();
+
+        // Return JSON response with the list of trips
+        return response()->json(['trip' => $trip]);;
+    }
+
+
+    public function deleteTrip(Request $request, $id)
+    {
+        // Find the trip by ID
+        $trip = Trip::findOrFail($id);
+
+        // Delete the trip
+        $trip->delete();
+
+        // Return JSON response with success message
+        return response()->json(['message' => 'Trip deleted successfully']);
+    }
+
+    public function deleteUser(Request $request, $id)
+    {
+        // Find the user by ID
+        $user = User::findOrFail($id);
+
+        // Delete the user
+        $user->delete();
+
+        // Return JSON response with success message
+        return response()->json(['message' => 'User deleted successfully']);
+    }
 }
